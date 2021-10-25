f0709c4bb64c4809e1335dc32288e221b28df635
italolelis <italolelis@gmail.com>
2021-10-25 17:48:27 +0200

    Add migrations for postgres and redshift

---
+++ configs/migrations/postgres/20201201132418_init.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE fourkeys.events_raw (
+    id VARCHAR NOT NULL,
+    event_type VARCHAR NOT NULL,
+    metadata JSONB NOT NULL,
+    time_created TIMESTAMP NOT NULL,
+    signature VARCHAR NOT NULL,
+    msg_id  VARCHAR NOT NULL,
+    source  VARCHAR NOT NULL,
+    CONSTRAINT "pk_events_raw_id" PRIMARY KEY ("signature")
+);
---
+++ configs/migrations/redshift/20201201132418_init.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE fourkeys.events_raw;
