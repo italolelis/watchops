2cc13ae7dc8a0c2708364d869c1409679c57b31e
italolelis <italolelis@gmail.com>
2021-10-25 17:59:40 +0200

    Add opsgenie provider

---
+++ internal/app/provider/opsgenie/parser.go
@@ -0,0 +1,39 @@
+package opsgenie
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/italolelis/fourkeys/internal/app/provider"
+)
+
+const (
+	sourceType = "opsgenie"
+)
+
+type opsgenieEvent struct {
+	Action string `json:"action"`
+}
+
+// Parser is the GitHub webhook parser.
+type Parser struct{}
+
+// GetName returns the parser name.
+func (p *Parser) GetName() string {
+	return sourceType
+}
+
+// Parse parses the incoming webhook.
+func (p *Parser) Parse(headers map[string][]string, payload []byte) (provider.Event, error) {
+	var e opsgenieEvent
+	if err := json.Unmarshal(payload, &e); err != nil {
+		return provider.Event{}, fmt.Errorf("failed to unmarshal metadata: %w", err)
+	}
+
+	return provider.Event{
+		EventType: e.Action,
+		Signature: "",
+		Source:    sourceType,
+		Metadata:  payload,
+	}, nil
+}
