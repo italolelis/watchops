3a3b0c4465fae1a9c146b011d928730d2646045d
italolelis <italolelis@gmail.com>
2021-11-04 16:15:35 +0000

    Add message broker configs

--- internal/app/subscriber/subscriber.go
+++ internal/app/subscriber/subscriber.go
@@ -13,17 +13,27 @@ type (
 	Subscriber interface {
 		Subscribe(ctx context.Context, streamName string, fn func(ctx context.Context, payload []byte, headers map[string][]string) error) error
 	}
+
+	// Config is the configuration for the subscriber.
+	Config struct {
+		Driver  string
+		Kinesis struct {
+			Endpoint   string
+			Region     string
+			StreamName string
+		}
+	}
 )
 
-func Build(ctx context.Context, driver string, configs map[string]string) (Subscriber, error) {
+func Build(ctx context.Context, driver string, cfg Config) (Subscriber, error) {
 	switch driver {
 	case "awslambda":
 		return awslambda.NewSubscriber(ctx)
 	case "kinesis":
 		return kinesis.NewSubscriber(ctx, kinesis.SessionConfig{
-			Endpoint:   configs["endpoint"],
-			Region:     configs["region"],
-			StreamName: configs["stream_name"],
+			Endpoint:   cfg.Kinesis.Endpoint,
+			Region:     cfg.Kinesis.Region,
+			StreamName: cfg.Kinesis.StreamName,
 		})
 	default:
 		return nil, errors.New("driver not supported. Please use one of the supported ones: awslambda, or kinesis")
