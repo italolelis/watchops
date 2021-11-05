dec4724a010a1c2cec3115f8e2e59b6e9799b688
italolelis <italolelis@gmail.com>
2021-11-05 17:15:10 +0000

    Adsd migrations to redshift

---
+++ configs/migrations/redshift/20211105112835_subscriber_checkpoint.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE kinesis_consumer (
+	namespace VARCHAR NOT NULL,
+	shard_id VARCHAR NOT NULL,
+	sequence_number numeric NOT NULL,
+	CONSTRAINT kinesis_consumer_pk PRIMARY KEY (namespace, shard_id)
+);
