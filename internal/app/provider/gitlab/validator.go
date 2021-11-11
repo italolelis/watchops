4dbcddd9fc213ec66ed5ad3553f544e14a999787
italolelis <italolelis@gmail.com>
2021-11-11 20:46:33 +0000

    Add GitLab webhook validator

---
+++ internal/app/provider/gitlab/validator.go
@@ -0,0 +1,41 @@
+package gitlab
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+)
+
+const tokenHeader = "X-Gitlab-Token"
+
+var (
+	// ErrEmptyToken is used the the incoming request has not token set
+	ErrEmptyToken = errors.New("the token can't be empty")
+	// ErrInvalidToken is used the the incoming request has an invalid token
+	ErrInvalidToken = errors.New("the token is invalid")
+)
+
+type Validator struct {
+	secret []byte
+}
+
+func NewValidator(secret string) *Validator {
+	return &Validator{secret: []byte(secret)}
+}
+
+func (v *Validator) Validate(r *http.Request) error {
+	token := []byte(r.Header.Get(tokenHeader))
+	if bytes.Equal(token, []byte("")) {
+		return ErrEmptyToken
+	}
+
+	if !bytes.Equal(token, v.secret) {
+		return ErrInvalidToken
+	}
+
+	return nil
+}
+
+func (g *Validator) IsSupported(incomingProvider string) bool {
+	return "gitlab" == incomingProvider
+}
