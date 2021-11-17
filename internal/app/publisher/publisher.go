a452faa900dce18c2935e67c34300b4630dbfc2a
italolelis <italolelis@gmail.com>
2021-11-17 20:24:23 +0000

    Add publisher abstract factory

--- internal/app/publisher/publisher.go
+++ internal/app/publisher/publisher.go
@@ -1,8 +1,30 @@
 package publisher
 
-import "context"
+import (
+	"context"
+	"errors"
 
-// Publisher represents the message publisher interface.
-type Publisher interface {
-	Publish(ctx context.Context, streamName string, data []byte) error
+	"github.com/italolelis/watchops/internal/app/publisher/kinesis"
+)
+
+type (
+	// Publisher represents the message publisher interface.
+	Publisher interface {
+		Publish(ctx context.Context, streamName string, data []byte) error
+	}
+
+	// Config is the configuration for the subscriber.
+	Config struct {
+		Driver  string `required:"true"`
+		Kinesis kinesis.SessionConfig
+	}
+)
+
+func Build(ctx context.Context, cfg Config) (Publisher, error) {
+	switch cfg.Driver {
+	case "kinesis":
+		return kinesis.NewPublisher(ctx, cfg.Kinesis)
+	default:
+		return nil, errors.New("driver not supported. Please use one of the supported ones: kinesis")
+	}
 }
