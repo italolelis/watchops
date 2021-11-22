799bdee77d3b6aa6f8abe46ac2d78057c4178315
solopiu <arianna_parisi@outlook.com>
2021-11-22 14:16:34 +0100

    created deployments view

---
+++ configs/migrations/redshift/1637586887590057000_create_deployments_view.up.sql
@@ -0,0 +1,15 @@
+CREATE VIEW deployments AS 
+   WITH deploys AS (
+      SELECT 
+      source,
+      json_extract_path_text(json_serialize(metadata), 'deployment', 'id') as deploy_id,
+      time_created,
+      CASE WHEN source like '%circleci%' then json_extract_path_text(json_serialize(metadata), 'workflow', 'id')
+           WHEN source like '%github%' then json_extract_path_text(json_serialize(metadata), 'deployment', 'sha')
+           WHEN source like '%gitlab%' then json_extract_path_text(json_serialize(metadata), 'commit', 'id') end as main_commit              
+      FROM fourkeys.events_raw
+      WHERE ((source like '%circleci%' and json_extract_path_text(json_serialize(metadata), 'status') = 'SUCCESS')
+            OR (source like 'github%' and event_type = 'deployment_status' and json_extract_path_text(json_serialize(metadata), 'deployment_status', 'state') = 'success')
+            OR (source like 'gitlab%' and event_type = 'pipeline' and json_extract_path_text(json_serialize(metadata), 'object_attributes', 'status') = 'success'))
+    )
+    select * from deploys
