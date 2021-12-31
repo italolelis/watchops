035eb8b1e186b69cc204edf5a5dd00d8b9e138e8
solopiu <arianna_parisi@outlook.com>
2021-12-31 09:16:32 +0100

    changed incidents and changes view

--- configs/migrations/redshift/20211215141537_create_incidents_view.up.sql
+++ configs/migrations/redshift/20211215141537_create_incidents_view.up.sql
@@ -3,26 +3,29 @@ CREATE OR REPLACE VIEW watchops.incidents AS (
     (
     SELECT 
         source,
-        CASE WHEN source LIKE 'github%' THEN json_extract_path_text(json_serialize(metadata), 'issue', 'number')
-            WHEN source LIKE 'gitlab%' AND event_type = 'note' THEN json_extract_path_text(json_serialize(metadata), 'object_attributes', 'noteable_id')
-            WHEN source LIKE 'gitlab%' AND event_type = 'issue' THEN json_extract_path_text(json_serialize(metadata), 'object_attributes', 'id')
-            WHEN source LIKE 'opsgenie%' THEN json_extract_path_text(json_extract_path_text(json_serialize(metadata), 'alert'), 'tinyId')
+        CASE WHEN source LIKE 'github%' THEN cast(metadata."issue"."number" as varchar) 
+            WHEN source LIKE 'gitlab%' AND event_type = 'note' THEN cast(metadata."object_attributes"."noteable_id" as varchar)
+            WHEN source LIKE 'gitlab%' AND event_type = 'issue' THEN cast(metadata."object_attributes"."id" as varchar)
+            WHEN source LIKE 'opsgenie%' THEN json_extract_path_text(json_serialize(metadata), 'alert', 'tinyId')
         end as incident_id,
-        CASE WHEN source LIKE 'github%' THEN TO_TIMESTAMP(json_extract_path_text(json_serialize(metadata), 'issue', 'created_at'), 'YYYY-MM-DD HH24:MI:SS')
-            WHEN source LIKE 'gitlab%' THEN TO_TIMESTAMP(json_extract_path_text(json_serialize(metadata), 'object_attributes', 'created_at'), 'YYYY-MM-DD HH24:MI:SS') 
+        CASE WHEN source LIKE 'github%' THEN TO_TIMESTAMP(cast(metadata."issue"."created_at" as varchar), 'YYYY-MM-DD HH24:MI:SS')
+            WHEN source LIKE 'gitlab%' THEN TO_TIMESTAMP(cast(metadata."object_attributes"."created_at" as varchar), 'YYYY-MM-DD HH24:MI:SS') 
             WHEN source LIKE 'opsgenie' THEN dateadd(ms, CAST(json_extract_path_text(json_extract_path_text(json_serialize(metadata), 'alert'), 'createdAt') AS bigint), '1970-01-01')
         end as time_created,
-        CASE WHEN source LIKE 'github%' THEN TO_TIMESTAMP(json_extract_path_text(json_serialize(metadata), 'issue', 'closed_at'), 'YYYY-MM-DD HH24:MI:SS')
-            WHEN source LIKE 'gitlab%' THEN TO_TIMESTAMP(json_extract_path_text(json_serialize(metadata), 'object_attributes', 'closed_at'), 'YYYY-MM-DD HH24:MI:SS') 
+        CASE WHEN source LIKE 'github%' THEN TO_TIMESTAMP(cast(metadata."issue"."closed_at" as varchar), 'YYYY-MM-DD HH24:MI:SS')
+            WHEN source LIKE 'gitlab%' THEN TO_TIMESTAMP(cast(metadata."object_attributes"."closed_at" as varchar), 'YYYY-MM-DD HH24:MI:SS') 
             WHEN source LIKE 'opsgenie' THEN dateadd(ms, CAST(json_extract_path_text(json_extract_path_text(json_serialize(metadata), 'alert'), 'updatedAt') AS bigint)/1000000, '1970-01-01')
         end as time_resolved,
         regexp_substr(json_serialize(metadata), '(?<=root cause: )(.*?)(?=\")', 1, 1,'p') as root_cause,
-        CASE WHEN source LIKE 'github%' THEN json_extract_path_text(json_serialize(metadata), 'issue', 'labels') like '%bug%'
-            WHEN source LIKE 'gitlab%' THEN json_extract_path_text(json_serialize(metadata), 'object_attributes', 'labels','title') like '%ncident%' 
+        CASE WHEN source LIKE 'github%' THEN cast(metadata."issue"."labels" as varchar) like '%bug%'
+            WHEN source LIKE 'gitlab%' THEN cast(metadata."object_attributes"."labels"."title" as varchar) like '%ncident%' 
             WHEN source LIKE 'opsgenie' THEN true
         end as bug
     FROM watchops.events_raw 
-    WHERE event_type LIKE '%issue%' OR (event_type = 'note' and json_extract_path_text(json_serialize(metadata), 'object_attributes', 'noteable_type') = 'Issue') OR source = 'opsgenie'
+    WHERE 
+    event_type LIKE '%issue%' 
+    OR (event_type = 'note' and cast(metadata."object_attributes"."noteable_type" as varchar) = 'Issue') 
+    OR source = 'opsgenie'
     )
     SELECT
     issue.source,
@@ -30,7 +33,8 @@ CREATE OR REPLACE VIEW watchops.incidents AS (
     date_trunc('second',MIN(issue.time_created)::timestamp) as time_created,
     date_trunc('second',MAX(issue.time_resolved)::timestamp) as time_resolved,
   	LISTAGG(issue.root_cause IGNORE NULLS) changes
-    FROM issue LEFT JOIN watchops.deployments d on d.main_commit = root_cause
+    FROM issue 
+    LEFT JOIN watchops.deployments d on d.main_commit = root_cause
     WHERE bug = true 
     and time_resolved >= TO_DATE('1900-01-01', 'YYYY-MM-DD') --filter null time_resolved 
     GROUP BY issue.source, issue.incident_id
--- configs/migrations/redshift/20211216172918_create_changes_view.up.sql
+++ configs/migrations/redshift/20211216172918_create_changes_view.up.sql
@@ -1,15 +1,14 @@
 CREATE OR REPLACE VIEW watchops.changes AS (
     WITH all_commits AS
     (
-    SELECT source, event_type, json_parse(json_serialize(metadata)) AS metadata
-    FROM watchops.events_raw
-    WHERE event_type = 'push'
-    and json_serialize(metadata) LIKE '%"commits":[%'                                         
+        SELECT source, event_type, metadata 
+        FROM watchops.events_raw
+        WHERE event_type = 'push'                                    
     ),
     commit AS
     (
-    SELECT index, i.source, i.event_type, element.timestamp AS time_created, element.id AS change_id
-    FROM all_commits AS i, i.metadata.commits AS element AT index
+        SELECT index, i.source, i.event_type, element.timestamp AS time_created, element.id AS change_id
+        FROM all_commits AS i, i.metadata.commits AS element AT index
     )
     SELECT source, event_type, TRIM('""' FROM CAST(change_id AS VARCHAR)) as change_id, date_trunc('second', TO_TIMESTAMP(CAST(time_created AS VARCHAR), 'YYYY-MM-DDTHH:MI:SS')::timestamp) AS time_created
     FROM commit
