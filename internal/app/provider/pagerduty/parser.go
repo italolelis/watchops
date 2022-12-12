328971af16c17516a5791c4080b35ea80f055f13
Italo Vietro <italolelis@gmail.com>
2022-12-12 19:00:51 +0000

    Add PagerDuty parser and validators

---
+++ internal/app/provider/pagerduty/parser.go
@@ -0,0 +1,55 @@
+package pagerduty
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/italolelis/watchops/internal/app/provider"
+)
+
+const (
+	sourceType = "pagerduty"
+)
+
+type event struct {
+	ID           string    `json:"id"`
+	EventType    string    `json:"event_type"`
+	ResourceType string    `json:"resource_type"`
+	OccurredAt   time.Time `json:"occurred_at"`
+}
+
+// Parser is the GitHub webhook parser.
+type Parser struct{}
+
+// GetName returns the parser name.
+func (p *Parser) GetName() string {
+	return sourceType
+}
+
+// Parse parses the incoming webhook.
+func (p *Parser) Parse(headers map[string][]string, payload []byte) (provider.Event, error) {
+	var e event
+	if err := json.Unmarshal(payload, &e); err != nil {
+		return provider.Event{}, fmt.Errorf("failed to unmarshal metadata: %w", err)
+	}
+
+	event := provider.Event{
+		EventType: e.EventType,
+		Signature: provider.GenerateSignature(payload),
+		Source:    sourceType,
+		Metadata:  payload,
+		MsgID:     headers["msg_id"][0],
+	}
+
+	switch event.EventType {
+	case "incident.triggered",
+		"incident.resolved":
+		event.ID = e.ID
+		event.TimeCreated = e.OccurredAt
+	default:
+		return provider.Event{}, &provider.UnkownTypeError{Type: event.EventType}
+	}
+
+	return event, nil
+}
